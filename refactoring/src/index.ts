a5ca4fb7dfaff5e8c973cb0b0eeb6183e4f9c794
조민혁 <chohyuk0725@gmail.com>
2022-10-09 09:04:41 +0900

    타입

--- refactoring/src/index.ts
+++ refactoring/src/index.ts
@@ -29,6 +29,7 @@ function statement(invoice: Invoice, plays: PlaysObj) {
       } | null;
       playID: PlayIds;
       audience: number;
+      amount: number | null;
     }[];
   } = { customer: "", performances: [] };
   statementData.customer = invoice.customer;
@@ -44,12 +45,46 @@ function statement(invoice: Invoice, plays: PlaysObj) {
       } | null;
       playID: PlayIds;
       audience: number;
-    } = { ...aPeformance, play: null };
+      amount: number | null;
+    } = { ...aPeformance, play: null, amount: null };
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
   function playFor(pref: Performance) {
     return (plays as unknown as PlaysObj)[pref.playID];
+  }
+  function amountFor(aPeformance: {
+    play: {
+      name: "Hamlet" | "As You Like It" | "Othello";
+      type: "comedy" | "tragedy";
+    } | null;
+    playID: PlayIds;
+    audience: number;
+    amount: number | null;
+  }) {
+    let thisAmount = 0;
+    if (!aPeformance.play) throw Error("check performances play");
+    switch (aPeformance.play.type) {
+      case "tragedy": //비극
+        thisAmount = 40000;
+        if (aPeformance.audience > 30) {
+          thisAmount += 1000 * (aPeformance.audience - 30);
+        }
+        break;
+
+      case "comedy": //희극
+        thisAmount = 30000;
+        if (aPeformance.audience > 20) {
+          thisAmount += 10000 + 500 * (aPeformance.audience - 20);
+        }
+        thisAmount += 300 * aPeformance.audience;
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르: ${aPeformance.play.type}`);
+    }
+    return thisAmount;
   }
 }
 function renderPlainText(
@@ -61,6 +96,7 @@ function renderPlainText(
       } | null;
       playID: PlayIds;
       audience: number;
+      amount: number | null;
     }[];
   },
   plays: PlaysObj
@@ -69,9 +105,9 @@ function renderPlainText(
 
   for (let pref of data.performances) {
     if (!pref.play) throw Error("check performances play");
-    result += `${pref.play.name}: ${usd(amountFor(pref) / 100)} (${
-      pref.audience
-    }석)`;
+    if (!pref.amount) throw Error("check performances amount");
+
+    result += `${pref.play.name}: ${usd(pref.amount)} (${pref.audience}석)`;
   }
   result += `총액: ${usd(totalAmount() / 100)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
@@ -80,7 +116,8 @@ function renderPlainText(
   function totalAmount() {
     let result = 0;
     for (let pref of data.performances) {
-      result += amountFor(pref);
+      if (!pref.amount) throw Error("check performances amount");
+      result += pref.amount;
     }
     return result;
   }
@@ -108,44 +145,13 @@ function renderPlainText(
     } | null;
     playID: PlayIds;
     audience: number;
+    amount: number | null;
   }) {
     let result = 0;
     result += Math.max(aPeformance.audience - 30, 0);
     if ("comedy" === aPeformance.play?.type)
       result += Math.floor(aPeformance.audience / 5);
     return result;
-  }
-
-  function amountFor(aPeformance: {
-    play: {
-      name: "Hamlet" | "As You Like It" | "Othello";
-      type: "comedy" | "tragedy";
-    } | null;
-    playID: PlayIds;
-    audience: number;
-  }) {
-    let thisAmount = 0;
-    if (!aPeformance.play) throw Error("check performances play");
-    switch (aPeformance.play.type) {
-      case "tragedy": //비극
-        thisAmount = 40000;
-        if (aPeformance.audience > 30) {
-          thisAmount += 1000 * (aPeformance.audience - 30);
-        }
-        break;
-
-      case "comedy": //희극
-        thisAmount = 30000;
-        if (aPeformance.audience > 20) {
-          thisAmount += 10000 + 500 * (aPeformance.audience - 20);
-        }
-        thisAmount += 300 * aPeformance.audience;
-        break;
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPeformance.play.type}`);
-    }
-    return thisAmount;
   }
 }
 
