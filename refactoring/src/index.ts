5b9dc9afa3785408d26bf1b097eecf25e0fa9e18
조민혁 <chohyuk0725@gmail.com>
2022-10-10 16:17:12 +0900

    htmlstatement 추가

--- refactoring/src/index.ts
+++ refactoring/src/index.ts
@@ -10,17 +10,32 @@ function renderPlainText(data) {
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점 \n`;
   return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("un-Us", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
 }
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1> \n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th>";
+  for (let pref of data.performances) {
+    result += `<tr><td>${pref.play.name}</td><td>(${pref.audience}석)</td></tr>`;
+    result += `<td>${usd(pref.amount)}</td>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+  return result;
+}
+function usd(aNumber) {
+  return new Intl.NumberFormat("un-Us", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
 console.log(statement(invoiceData[0], playsData));
