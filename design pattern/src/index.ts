6f02f5c572af33dbecc95b954b6c64490a7d5caf
조민혁 <chohyuk0725@gmail.com>
2022-10-12 01:23:40 +0900

    composition

--- design pattern/src/index.ts
+++ design pattern/src/index.ts
@@ -1,1 +1,56 @@
-console.log("hello");
+abstract class FlyBehavior {
+  fly() {}
+}
+
+abstract class QuackBehavior {
+  quack() {}
+}
+class Duck {
+  private flyBehavior;
+  private quackBehavior;
+  constructor(flyBehavior: FlyBehavior, quackBehavior: QuackBehavior) {
+    this.flyBehavior = flyBehavior;
+    this.quackBehavior = quackBehavior;
+  }
+
+  public fly() {
+    this.flyBehavior.fly();
+  }
+  public quack() {
+    this.quackBehavior.quack();
+  }
+}
+
+class FlyWithWings implements FlyBehavior {
+  fly() {
+    console.log("fly");
+  }
+}
+
+class FlyNoWay implements FlyBehavior {
+  fly() {
+    console.log("못날음");
+  }
+}
+
+class Quack implements QuackBehavior {
+  quack() {
+    console.log("quack");
+  }
+}
+
+class Squack implements QuackBehavior {
+  quack() {
+    console.log("Squack");
+  }
+}
+
+class MuteQuack implements QuackBehavior {
+  quack() {
+    console.log("MuteQuack");
+  }
+}
+
+const duck = new Duck(new FlyWithWings(), new MuteQuack());
+
+duck.fly();
