4478c724312e32a76175037673818a8afb5aa0e8
조민혁 <chohyuk0725@gmail.com>
2022-10-13 21:27:56 +0900

    사이즈 추가

--- design pattern/src/decoratorPattern.ts
+++ design pattern/src/decoratorPattern.ts
@@ -1,9 +1,16 @@
+type Size = "TALL" | "GRANDE" | "VENTI";
 abstract class Beberage {
   description = "제목없음";
+  size: Size = "TALL";
   getDescription() {
     return this.description;
   }
-
+  setSize(size: Size) {
+    this.size = size;
+  }
+  getSize() {
+    return this.size;
+  }
   cost() {
     return 0;
   }
@@ -22,27 +29,60 @@ abstract class CondimentDecorator extends Beberage {
 
 class Espresso implements Beberage {
   description: string;
-  constructor() {
+  size: Size;
+  constructor(size: Size = "TALL") {
     this.description = "에스프레소";
+    this.size = size;
   }
   getDescription() {
     return this.description;
   }
+  setSize(size: Size) {
+    this.size = size;
+  }
+  getSize() {
+    return this.size;
+  }
   cost() {
-    return 1.99;
+    switch (this.size) {
+      case "TALL":
+        return 1.99;
+      case "GRANDE":
+        return 2.2;
+      case "VENTI":
+        return 2.5;
+    }
   }
 }
 
 class HouseBlend implements Beberage {
   description: string;
-  constructor() {
+  size: Size;
+
+  constructor(size: Size = "TALL") {
     this.description = "하우스 블렌드 커피";
+
+    this.size = size;
   }
+
   getDescription() {
     return this.description;
   }
+  setSize(size: Size) {
+    this.size = size;
+  }
+  getSize() {
+    return this.size;
+  }
   cost() {
-    return 1.99;
+    switch (this.size) {
+      case "TALL":
+        return 1.99;
+      case "GRANDE":
+        return 2.2;
+      case "VENTI":
+        return 2.5;
+    }
   }
 }
 
@@ -58,14 +98,14 @@ class Mocha extends Beberage implements CondimentDecorator {
   }
 
   cost() {
-    return this.beberage.cost() + 0.2;
+    return this.beberage.cost() + 0.4;
   }
 }
 
 class StarbuzzCoffee {
   beberage: Beberage;
   constructor() {
-    this.beberage = new Espresso();
+    this.beberage = new Espresso("VENTI");
     this.beberage = new Mocha(this.beberage);
     this.beberage = new Mocha(this.beberage);
     console.log(`${this.beberage.getDescription()} ${this.beberage.cost()}`);
